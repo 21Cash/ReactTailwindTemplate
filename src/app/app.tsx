e841cf14912b73aa8ae89a88961faa1658c8f958
21Cash <21ThousandProof@gmail.com>
2023-12-13 11:29:57 +0530

    Initial Commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+import Homepage from "components/Homepage/Homepage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Homepage/Homepage.tsx
@@ -0,0 +1,9 @@
+const Homepage = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-4xl">Homepage</h1>
+    </div>
+  );
+};
+
+export default Homepage;
